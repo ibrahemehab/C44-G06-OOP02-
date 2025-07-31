bc3191edbb0eb34a386f0edc257ed8ad5e797007
esmaelehab <63879362+esmaelehab@users.noreply.github.com>
2025-07-31 15:55:02 +0300

    question 1

--- assignment/Program.cs
+++ assignment/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace assignment
+﻿using static assignment.person;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region examples
+            //Point point = new Point();
+            //Console.WriteLine(point.x);
+            //Point point2 = new Point(8);
+            //Console.WriteLine(point2.x);
+            //object obj = point;
+            //Point2 point3 = new Point2();
+            //point3.x = 66;
+            //Console.WriteLine(point3.x); 
+            #endregion
+            //Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data.
+            //Then, write a C# program to display the details of all the persons in the array.
+            person[] people = new person[3];
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine($"enter person number {i + 1} data :");
+                Console.WriteLine($"his name :");                                       
+                string name = Console.ReadLine();
+                Console.WriteLine($"his age :");
+                int age = int.Parse(Console.ReadLine());
+                people[i] = new person(age, name);
+            }
+            person people2 = new person();
+            people2.displayAllObjects(people);
         }
     }
 }
---
+++ assignment/point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    #region struct person 
+    public struct person
+    {
+        private int age;
+        private string name;
+        //public string Name
+        //{
+        //    get { return name; }   // Getter
+        //    set { name = value; }  // Setter
+        //}
+        //public int Age
+        //{
+        //    get { return age; }    // Getter
+        //    set
+        //    {
+        //        if (value >= 0)    // Example validation
+        //            age = value;
+        //    }
+        //}
+        public person(int AGE, string NAME)
+        {
+            age = AGE;
+            name = NAME;
+        }
+        public void displayAllObjects(person[] people)
+        {
+            foreach (person person in people)
+            {
+                Console.WriteLine(person.name + " " + person.age);
+            }
+        }
+
+        
+
+    } 
+    #endregion
+
+
+
+
+
+}
